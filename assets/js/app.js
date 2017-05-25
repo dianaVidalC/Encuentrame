ab9519b59f10050ccf9ff57058e50fd7edb25de7
Erika Vidal <diann852@gmail.com>
2017-05-25 18:39:27 -0500

    primer commit

---
+++ assets/js/app.js
@@ -0,0 +1,31 @@
+function initMap() {
+    var laboratoriaLima = {lat: -12.1042, lng: -77.0299};
+    var map = new google.maps.Map(document.getElementById("map"), {
+        zoom: 18,
+        center: laboratoriaLima
+    });
+    var markadorLaboratoria = new google.maps.Marker({
+        position: laboratoriaLima,
+        map: map
+    });
+    function buscar(){
+        if(navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(funcionExito, funcionError);
+        }
+    }
+    document.getElementById("encuentrame").addEventListener("click", buscar);
+    var latitud,longitud;
+    var funcionExito = function(posicion) {
+        latitud = posicion.coords.latitude;
+        longitud = posicion.coords.longitude;
+        var miUbicacion = new google.maps.Marker({
+            position: {lat:latitud, lng:longitud},
+            map: map
+        });
+        map.setZoom(18);
+        map.setCenter({lat:latitud, lng:longitud});
+    }
+}
+var funcionError = function (error) {
+    alert("Tenemos un problema con encontrar tu ubicación");
+}
